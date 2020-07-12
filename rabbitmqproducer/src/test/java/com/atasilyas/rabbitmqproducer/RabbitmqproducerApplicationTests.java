4e47e3187260f17b8a9c09ef8be3b612192b2287
atasilyas <ilyas.atas@hotmail.com>
2020-07-12 16:31:25 +0300

    producer application created.

---
+++ rabbitmqproducer/src/test/java/com/atasilyas/rabbitmqproducer/RabbitmqproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atasilyas.rabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqproducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
